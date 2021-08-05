c610c0ac79702d2fdb1c17e89abadd0af8d889f5
Tareq69 <mtareq880@gmail.com>
2021-08-05 13:14:21 +0600

    gitignore finally updated

--- mysite/mysite/secret_settings.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'django-insecure-xzyaq=i2wz0(1)4q30_q&%l6q_z3v2lg!iga7tavw_yr^^f@&%'
--- userauthprac/userauthprac/settings.py
+++ userauthprac/userauthprac/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from secret_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!j=3o=x&k9_j(xvs8xj2!q&)#u463p*nb2v#_6k)#mo7q-t^ib'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
